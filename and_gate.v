3bae3c83d24aae7055e7c5954559f312c873c52d
Dwayne-Rodrigues <dwaynerodrigues18@gmail.com>
2026-03-30 13:27:23 +0530

    Add files via upload

---
+++ and_gate.v
@@ -0,0 +1,19 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Author: Dwayne E. Rodrigues
+// Module Name: and_gate
+// Project Name: the_rtl_compendium
+// Description: Basic 2-input AND logic primitive.
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module and_gate(
+    input wire a,   // Input A
+    input wire b,   // Input B   
+    output wire y   // Output Result (Y = A & B)
+    );
+    
+    // Continuous assignment for combinational logic
+    assign y = a & b;
+    
+endmodule
