1edef876533f2fdccaa43a09f06b7780a8e05476
Dwayne-Rodrigues <dwaynerodrigues18@gmail.com>
2026-04-03 20:27:31 +0530

    and_gate.vhd

--- 01_Logic_Gates/AND_Gate/vhdl/and_gate.vhd
+++ 01_Logic_Gates/AND_Gate/vhdl/and_gate.vhd
@@ -10,7 +10,7 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity and_gate is
-    Port ( a : in  STD_LOGIC; -- Input A
+    port ( a : in  STD_LOGIC; -- Input A
            b : in  STD_LOGIC; -- Input B
            y : out STD_LOGIC  -- Output Result (Y = A AND B)
          );
